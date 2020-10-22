09da8ebfc218137084f0e7e072c77dd87e7210fc
Paweł <p.nowecki@gmail.com>
2020-10-22 15:09:09 +0200

    Dodane dane

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+// import {
+//   BrowserRouter,
+//   Route,
+//   Switch,
+// } from "react-router-dom";
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+import { Route, useHistory } from 'react-router-dom';
 
 const API_URL = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
 
@@ -11,7 +18,12 @@ const SearchInfo = () => {
     setNumber(e.target.value)
   }
   // console.log(number)
+  let history = useHistory();
+  let url = ` ${API_URL}${number}`;
 
+  function handleClick() {
+    history.push(url);
+  }
 
   const BookInfo = ({ number }) => {
     const [items, setItems] = useState([]);
@@ -31,7 +43,7 @@ const SearchInfo = () => {
           if (data.items) {
             setItems(data.items);
           } else {
-            setError("BRAK");
+            setError("Brak pozycji, sprawdź poprawność numeru ISBN");
           }
         })
         .catch(() => {
@@ -45,37 +57,80 @@ const SearchInfo = () => {
     if (items.length === 0) {
       return "Trwa ładowanie";
     }
+    // if (items.volumeInfo.imageLinks.length === 0){
+    //   return "Brak zdjęcia"
+    // }
     return (
-      <div>
+      <div className="MainBox">
         <ul>
           {items.map(item => {
-            return <li key={item.id}>{item.volumeInfo.title}</li>;
+            return (
+              <div className="TitleName">
+                <img src={item.volumeInfo.imageLinks.thumbnail} alt="okładka"></img>
+                <li key={item.id}>
+                  <span>Tytuł:</span> {item.volumeInfo.title}<br />
+                  <span>Autor:</span> {item.volumeInfo.authors}<br />
+                  <span>Data publikacji:</span> {item.volumeInfo.publishedDate}  <br />
+                  <span>Opis:</span> {item.volumeInfo.description}  <br />
+                  <span>Kategoria:</span> {item.volumeInfo.categories}  <br />
+
+                </li>
+              </div>
+            );
           })}
         </ul>
       </div>
     );
   };
 
+
   return (
     <>
-      <div>
+      <div className="MainBox">
         <label>
           <input
+            placeholder="Wpisz numer ISBN"
             type="text"
             value={number}
-            onChange={handleChangeNumber}>
+            // onChange={handleClick}
+            onChange={handleChangeNumber}
+          >
           </input>
+          <button type="button"
+            onClick={handleClick}
+          >Szukaj</button>
         </label>
       </div>
-      <BookInfo number={number} />
+      {number ? (
+        <BookInfo number={number} />
+      ) : (
+          <div className="MainBox">
+            Wpisz numer ISBN, aby wyszukać swoją ulubioną książkę
+          </div>
+        )}
     </>
   );
 }
 
 
 
-export default function App() {
+export default function App({ API_URL, number }) {
+  //   let url = number;
+  // let newUrl = API_URL ;
   return (
-    <SearchInfo />
+    // <BrowserRouter>
+    //   <Switch>
+    <Router>
+      {/* <Route> */}
+      <Link to={`/${number}`} value={number} />
+      <SearchInfo>
+        <Route path="/:number" component={SearchInfo}></Route>
+      </SearchInfo>
+
+      {/* </Link> */}
+      {/* </Route> */}
+    </Router>
+
+
   )
 }
